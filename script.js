4a27d1eeb49fe076bb261a82fa5b5b40ed9eeec4
Azmyba <90493491+Azmyba@users.noreply.github.com>
2025-12-16 16:52:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ const accordions = document.querySelectorAll(".accordion");
 
 accordions.forEach(btn => {
   btn.addEventListener("click", () => {
-    btn.classList.toggle("active");
     const panel = btn.nextElementSibling;
-
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+    panel.style.display = panel.style.display === "block" ? "none" : "block";
   });
 });
